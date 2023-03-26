3e08fa8671948c272220bbc57c52b2b8070c270a
matt roche <matt.roche@gmail.com>
2023-03-26 14:42:00 -0500

    phase 1 tasklister mini project complete deliverables and first stretch deliverable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+  })
 });
+
+function buildToDo(toDo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  li.textContent = `${toDo}    `
+  li.appendChild(btn)
+  list.appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
+//function buildToDo(descriptionField) {
+//  let li = document.createElement('li')
+//  li.textContent = `${descriptionField}`
+//  li.appendChild(todoTasks) 
+//}
